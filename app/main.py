3ae2284acf0a52db78c15ea69a8e15f55f75c77c
Yashwanth Reddy V <yash@Yashs-MacBook.local>
2022-06-22 13:38:09 +0530

    it5

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rediva-lifestyle.web.app/","http://localhost:3000/"],
+    allow_origins=["*"],
     allow_headers=["*"],
     allow_methods=["*"],
     allow_credentials=True)
