9f1ddfc2a035081614410cf2ab238579a8b8b2e4
Yashwanth Reddy V <yash@Yashs-MacBook.local>
2022-08-05 15:04:51 +0530

    make it better

--- app/routers/image.py
+++ app/routers/image.py
@@ -1,18 +1,18 @@
-from io import BytesIO
-from fastapi import APIRouter, UploadFile
-from fastapi.responses import StreamingResponse
-from PIL import Image
-import base64
+# from io import BytesIO
+# from fastapi import APIRouter, UploadFile
+# from fastapi.responses import StreamingResponse
+# from PIL import Image
+# import base64
 
-router = APIRouter( prefix="/image" ,tags=["Image"])
+# router = APIRouter( prefix="/image" ,tags=["Image"])
 
-@router.post("/webp")
-def convert_to_webp(img: UploadFile):
+# @router.post("/webp")
+# def convert_to_webp(img: UploadFile):
 
-    res = BytesIO()
+#     res = BytesIO()
 
-    image = Image.open(img.file)
-    image.save(res, format="webp")
-    res.seek(0)
+#     image = Image.open(img.file)
+#     image.save(res, format="webp")
+#     res.seek(0)
 
-    return StreamingResponse(res,media_type="image/webp")
+#     return StreamingResponse(res,media_type="image/webp")
