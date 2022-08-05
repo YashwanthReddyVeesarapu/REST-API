f7c1076e48a67c6522d54993ca1dbf383018feb8
Yashwanth Reddy V <yash@Yashs-MacBook.local>
2022-08-05 15:02:54 +0530

    make it better

--- app/routers/redflix.py
+++ app/routers/redflix.py
@@ -1,12 +1,9 @@
 from datetime import datetime, timedelta
-from typing import Union
 from fastapi import APIRouter, HTTPException
-from jose import JWTError, jwt
 
 from .. import models
 
-from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 
 preusername = "red"
@@ -16,43 +13,9 @@ router = APIRouter(prefix="/redflix", tags=['Redflix'])
 
 SECRET_KEY = "09d25e-094f-"+ str(datetime.utcnow().date()) + "aa-6ca2-556" + str(datetime.utcnow().hour)
 print(SECRET_KEY)
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password):
-    return pwd_context.hash(password)
 
-def get_user(username):
-    if preusername == username:
-        return True
-    else:
-        return False
-
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(data: dict, expires_delta: Union[timedelta,None] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
 
 
 @router.post('/login')
